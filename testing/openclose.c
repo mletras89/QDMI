9baf644040ae782f33e2d5f1b2dc71ea121ed76a
Martin Schulz <schulzm@in.tum.de>
2024-01-12 06:00:46 +0100

    added missing testing template

---
+++ testing/openclose.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <qdmi.h>
+
+#define CHECK_ERR(a,b) { if (a!=QDMI_SUCCESS) { printf("Error: %i at %s\n",a,b); return 1; }}
+
+int main(int agrc, char** argv)
+{
+    return 0;
+}
+
+
