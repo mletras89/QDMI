b597e9b56e51a8a5966a934fc0920b70fe180a59
Jorge Echavarria <jorge.echavarria@lrz.de>
2024-01-26 11:28:37 +0000

    Getting rid of testing just temporarily

--- cmake/Findqinfo.cmake
+++ cmake/Findqinfo.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     qinfo
     GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/QInfo.git
-    GIT_TAG main  # Replace with the desired branch, tag, or commit
+    GIT_TAG testing
 )
 
 FetchContent_MakeAvailable(qinfo)
