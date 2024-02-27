98d6ccfcb33059cc000c772a900c4d3555d26aa6
Jorge Echavarria <jorge.echavarria@lrz.de>
2024-02-27 20:19:32 +0000

    Fixed error with WMIs topology

--- src/qdmi_backend_wmi.c
+++ src/qdmi_backend_wmi.c
@@ -219,19 +219,19 @@ int QDMI_set_coupling_mapping(QDMI_Device dev, int qubit_index, QDMI_Qubit qubit
     qubit->index = qubit_index;
 
     switch (qubit_index) {
-        case 1:
+        case 0:
             qubit->coupling_mapping = (QDMI_qubit_index*)malloc(2 * sizeof(QDMI_qubit_index));
             qubit->coupling_mapping[0] = 1;
             qubit->coupling_mapping[1] = 2;
             qubit->size_coupling_mapping = 2;
             break;
-        case 2:
+        case 1:
             qubit->coupling_mapping = (QDMI_qubit_index*)malloc(2 * sizeof(QDMI_qubit_index));
             qubit->coupling_mapping[0] = 0;
             qubit->coupling_mapping[1] = 2;
             qubit->size_coupling_mapping = 2;
             break;
-        case 3:
+        case 2:
             qubit->coupling_mapping = (QDMI_qubit_index*)malloc(3 * sizeof(QDMI_qubit_index));
             qubit->coupling_mapping[0] = 0;
             qubit->coupling_mapping[1] = 1;
