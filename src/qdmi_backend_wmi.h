47d87c39e6dcc952c9101b73b9fc71eac5c303d2
Jorge Echavarria <jorge.echavarria@lrz.de>
2024-05-15 12:35:52 +0000

    Fixed WMIs supported gate set

--- src/qdmi_backend_wmi.h
+++ src/qdmi_backend_wmi.h
@@ -6,13 +6,12 @@
 
 const char *gate_set[] = 
 {
-    "__quantum__qis__barrier__body", 
-    "__quantum__qis__cz__body", 
-    "__quantum__qis__rz__body",      
+    "__quantum__qis__id__body", 
     "__quantum__qis__x__body",    
-    "__quantum__qis__sx__body",   
     "__quantum__qis__y__body",
-    "__quantum__qis__z__body",       
+    "__quantum__qis__sx__body",   
+    "__quantum__qis__rz__body",      
+    "__quantum__qis__mz__body",       
 };
 
 #endif // QDMIBACKENDWMI_H
