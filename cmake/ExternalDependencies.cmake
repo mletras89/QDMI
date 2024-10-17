de417f0a2e79e8ddb41aee2f8e768f1fc1590c14
burgholzer <burgholzer@me.com>
2024-10-17 20:30:15 +0200

    🔥 remove QInfo
    Signed-off-by: burgholzer <burgholzer@me.com>

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -8,37 +8,6 @@ include(FetchContent)
 include(CMakeDependentOption)
 set(FETCH_PACKAGES "")
 
-# cmake-format: off
-set(QINFO_VERSION 0.2.0
-    CACHE STRING "QInfo version")
-set(QINFO_REV "c4e06b974d9ddd35964f41d7e40a17dd0f4b3539"
-    CACHE STRING "QInfo identifier (tag, branch or commit hash)")
-set(QINFO_REPO_OWNER "Munich-Quantum-Software-Stack"
-    CACHE STRING "QInfo repository owner (change when using a fork)")
-# cmake-format: on
-
-# enable install instructions for qinfo whenever QDMI shall be installed
-cmake_dependent_option(QINFO_INSTALL "Install QInfo library" ON "QDMI_INSTALL"
-                       OFF)
-
-if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
-  FetchContent_Declare(
-    qinfo
-    GIT_REPOSITORY https://github.com/${QINFO_REPO_OWNER}/QInfo.git
-    GIT_TAG ${QINFO_REV}
-    FIND_PACKAGE_ARGS ${QINFO_VERSION})
-  list(APPEND FETCH_PACKAGES qinfo)
-else()
-  find_package(qinfo ${QINFO_VERSION} QUIET)
-  if(NOT qinfo_FOUND)
-    FetchContent_Declare(
-      qinfo
-      GIT_REPOSITORY https://github.com/${QINFO_REPO_OWNER}/QInfo.git
-      GIT_TAG ${QINFO_REV})
-    list(APPEND FETCH_PACKAGES qinfo)
-  endif()
-endif()
-
 if(BUILD_QDMI_TESTS)
   set(gtest_force_shared_crt
       ON
--- include/qdmi/return_codes.h
+++ include/qdmi/return_codes.h
@@ -10,8 +10,6 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #pragma once
 
-#include <qinfo.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -65,22 +63,6 @@ static inline int QDMI_is_Warning(const int err) { return err > QDMI_SUCCESS; }
  * @return 1 if the return value is an error, 0 otherwise.
  */
 static inline int QDMI_is_Error(const int err) { return err < QDMI_SUCCESS; }
-
-static inline int QDMI_translate_qinfo_error(const int err) {
-  if (err == QINFO_SUCCESS) {
-    return QDMI_SUCCESS;
-  }
-
-  if (err == QINFO_ERROR_OUTOFMEM) {
-    return QDMI_ERROR_OUT_OF_MEM;
-  }
-
-  if (QInfo_is_Warning(err)) {
-    return QDMI_WARN_GENERAL;
-  }
-
-  return QDMI_ERROR_FATAL;
-}
 
 #ifdef __cplusplus
 } // extern "C"
