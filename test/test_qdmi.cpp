deae18509d2b736b0a1c794bec8a718aa6352386
burgholzer <burgholzer@me.com>
2024-10-20 16:52:47 +0200

    ✅ improved testing for example backends
    Signed-off-by: burgholzer <burgholzer@me.com>

--- test/test_qdmi.cpp
+++ test/test_qdmi.cpp
@@ -12,6 +12,8 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #include <cstdlib>
 #include <gtest/gtest.h>
 #include <string>
+#include <utility>
+#include <vector>
 
 // Instantiate the test suite with different parameters
 INSTANTIATE_TEST_SUITE_P(
@@ -34,137 +36,129 @@ INSTANTIATE_TEST_SUITE_P(
       return filename;
     });
 
-class QDMITest : public ::testing::Test {
-protected:
-  void SetUp() override {
-    ASSERT_TRUE(QDMI_is_Success(QDMI_session_alloc(&session)))
-        << "Failed to allocate session";
-  }
+// todo: would it make sense to enforce some of these tests in the
+//  `test_impl.hpp` file? These are very general and I think it boils down to
+//  whether we want to enforce that all implementations provide certain
+//  functionality or not.
 
-  void TearDown() override { QDMI_session_free(session); }
-
-  QDMI_Session session = nullptr;
-  QDMI_Device device = nullptr;
-  const std::string c_backend_name =
-      std::string("../examples/libc_backend") + Shared_library_file_extension();
-  const std::string cxx_backend_name =
-      std::string("../examples/libcxx_backend") +
-      Shared_library_file_extension();
-};
-
-TEST_F(QDMITest, OpenDevice) {
-  ASSERT_TRUE(QDMI_is_Success(QDMI_session_open_device(
-      session, c_backend_name.c_str(), QDMI_DEVICE_MODE_READ_WRITE, &device)))
-      << "Failed to open device";
-}
-
-TEST_F(QDMITest, OpenMultipleDevices) {
-  ASSERT_TRUE(QDMI_is_Success(QDMI_session_open_device(
-      session, c_backend_name.c_str(), QDMI_DEVICE_MODE_READ_WRITE, &device)))
-      << "Failed to open first device";
-  QDMI_Device device2 = nullptr;
-  ASSERT_TRUE(QDMI_is_Success(
-      QDMI_session_open_device(session, cxx_backend_name.c_str(),
-                               QDMI_DEVICE_MODE_READ_WRITE, &device2)))
-      << "Failed to open second device";
-}
-
-TEST_F(QDMITest, OpenDeviceReadOnly) {
-  ASSERT_TRUE(QDMI_is_Success(QDMI_session_open_device(
-      session, c_backend_name.c_str(), QDMI_DEVICE_MODE_READ_ONLY, &device)))
-      << "Failed to open first device";
-  QDMI_Job job = nullptr;
-  ASSERT_EQ(QDMI_control_submit_qasm(device, "OPENQASM 2.0;", 1, &job),
-            QDMI_ERROR_PERMISSION_DENIED);
-}
-
-TEST_F(QDMITest, QueryNumQubits5) {
-  ASSERT_TRUE(QDMI_is_Success(QDMI_session_open_device(
-      session, c_backend_name.c_str(), QDMI_DEVICE_MODE_READ_WRITE, &device)))
-      << "Failed to open device";
+int Query_num_qubits(QDMI_Device device) {
   int num_qubits = 0;
-  ASSERT_TRUE(QDMI_is_Success(
+  EXPECT_TRUE(QDMI_is_Success(
       QDMI_query_device_property_int(device, QDMI_NUM_QUBITS, &num_qubits)))
       << "Failed to query number of qubits";
-  ASSERT_EQ(num_qubits, 5);
+  return num_qubits;
 }
 
-TEST_F(QDMITest, QueryNumQubits7) {
-  ASSERT_TRUE(QDMI_is_Success(QDMI_session_open_device(
-      session, cxx_backend_name.c_str(), QDMI_DEVICE_MODE_READ_WRITE, &device)))
-      << "Failed to open device";
-  int num_qubits = 0;
-  ASSERT_TRUE(QDMI_is_Success(
-      QDMI_query_device_property_int(device, QDMI_NUM_QUBITS, &num_qubits)))
-      << "Failed to query number of qubits";
-  ASSERT_EQ(num_qubits, 7);
-}
-
-TEST_F(QDMITest, QueryDeviceName) {
-  char *value = nullptr;
-  QDMI_session_open_device(session, c_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
-  QDMI_query_device_property_string(device, QDMI_NAME, &value);
-  ASSERT_STREQ(value, "Backend with 5 qubits");
-}
-
-TEST_F(QDMITest, QueryDeviceName7) {
-  char *value = nullptr;
-  QDMI_session_open_device(session, cxx_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
-  QDMI_query_device_property_string(device, QDMI_NAME, &value);
-  ASSERT_STREQ(value, "Backend with 7 qubits");
-}
-
-TEST_F(QDMITest, QueryDeviceVersion) {
-  char *value = nullptr;
-  QDMI_session_open_device(session, c_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
-  QDMI_query_device_property_string(device, QDMI_DEVICE_VERSION, &value);
-  ASSERT_STREQ(value, "0.1.0");
-}
-
-TEST_F(QDMITest, QueryDeviceLibraryVersion) {
-  char *value = nullptr;
-  QDMI_session_open_device(session, c_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
-  QDMI_query_device_property_string(device, QDMI_LIBRARY_VERSION, &value);
-  ASSERT_STREQ(value, "0.2.0");
-}
-
-TEST_F(QDMITest, QueryAvgT1Time) {
-  double value = 0;
-  QDMI_session_open_device(session, c_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
-  QDMI_query_device_property_double(device, QDMI_AVG_T1_TIME, &value);
-  ASSERT_DOUBLE_EQ(value, 1000);
-}
-
-TEST_F(QDMITest, QueryAvgT2Time) {
-  double value = 0;
-  QDMI_session_open_device(session, c_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
-  QDMI_query_device_property_double(device, QDMI_AVG_T2_TIME, &value);
-  ASSERT_DOUBLE_EQ(value, 100000);
-}
-
-TEST_F(QDMITest, QueryGateSet) {
+std::vector<std::string> Query_gate_set(QDMI_Device device) {
   char **gate_set = nullptr;
   int num_gates = 0;
-  const std::array<std::string, 4> value = {"cz", "rx", "ry", "rz"};
-  QDMI_session_open_device(session, c_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
-  QDMI_query_device_property_string_list(device, QDMI_GATE_SET, &gate_set,
-                                         &num_gates);
-  for (std::size_t i = 0; i < static_cast<std::size_t>(num_gates); i++) {
-    ASSERT_STREQ(gate_set[i], value[i].c_str());
+  EXPECT_TRUE(QDMI_is_Success(QDMI_query_device_property_string_list(
+      device, QDMI_GATE_SET, &gate_set, &num_gates)))
+      << "Failed to query gate set";
+  std::vector<std::string> gates;
+  gates.reserve(static_cast<size_t>(num_gates));
+  for (int i = 0; i < num_gates; i++) {
+    EXPECT_NE(gate_set[i], nullptr) << "Gate set contains a null pointer";
+    gates.emplace_back(gate_set[i]);
+  }
+  return gates;
+}
+
+std::vector<std::pair<int, int>> Query_coupling_map(QDMI_Device device) {
+  int *coupling_map = nullptr;
+  int num_couplings = 0;
+  EXPECT_TRUE(QDMI_is_Success(QDMI_query_device_property_int_list(
+      device, QDMI_COUPLING_MAP, &coupling_map, &num_couplings)))
+      << "Failed to query coupling map";
+  EXPECT_TRUE(num_couplings % 2 == 0) << "Coupling map is not a list of pairs";
+
+  const auto num_qubits = Query_num_qubits(device);
+  std::vector<std::pair<int, int>> coupling_pairs;
+  coupling_pairs.reserve(static_cast<size_t>(num_couplings / 2));
+  for (int i = 0; i < num_couplings; i += 2) {
+    EXPECT_TRUE(0 <= coupling_map[i] && coupling_map[i] < num_qubits)
+        << "Invalid qubit index in coupling map";
+    EXPECT_TRUE(0 <= coupling_map[i + 1] && coupling_map[i + 1] < num_qubits)
+        << "Invalid qubit index in coupling map";
+    coupling_pairs.emplace_back(coupling_map[i], coupling_map[i + 1]);
+  }
+  return coupling_pairs;
+}
+
+int Query_gate_num_qubits(QDMI_Device device, const std::string &gate) {
+  int num_qubits = 0;
+  EXPECT_TRUE(QDMI_is_Success(QDMI_query_operation_property_int(
+      device, gate.c_str(), nullptr, 0, QDMI_OPERATION_NUM_QUBITS,
+      &num_qubits)))
+      << "Failed to query number of qubits for gate " << gate;
+  return num_qubits;
+}
+
+TEST_P(QDMIImplementationTest, QueryNumQubits) {
+  ASSERT_GT(Query_num_qubits(device), 0);
+}
+
+TEST_P(QDMIImplementationTest, QueryGateSet) {
+  const auto gates = Query_gate_set(device);
+  ASSERT_GT(gates.size(), 0);
+  for (const auto &gate : gates) {
+    ASSERT_GT(gate.size(), 0);
   }
 }
 
-TEST_F(QDMITest, ToolCompile) {
-  QDMI_session_open_device(session, c_backend_name.c_str(),
-                           QDMI_DEVICE_MODE_READ_WRITE, &device);
+TEST_P(QDMIImplementationTest, QueryCouplingMap) {
+  const auto coupling_map = Query_coupling_map(device);
+  const auto num_qubits = Query_num_qubits(device);
+  if (num_qubits == 1) {
+    ASSERT_TRUE(coupling_map.empty());
+  } else {
+    ASSERT_GT(coupling_map.size(), 0);
+  }
+}
+
+TEST_P(QDMIImplementationTest, QueryGatePropertiesForEachGate) {
+  // for every gate in the gate set, query the duration of the gate
+  const auto num_qubits = Query_num_qubits(device);
+  const auto gates = Query_gate_set(device);
+  const auto coupling_map = Query_coupling_map(device);
+
+  for (const auto &gate : gates) {
+    const auto gate_num_qubits = Query_gate_num_qubits(device, gate);
+    double duration = 0;
+    double fidelity = 0;
+    if (gate_num_qubits == 1) {
+      for (int i = 0; i < num_qubits; i++) {
+        auto sites = std::array{i};
+        EXPECT_TRUE(QDMI_is_Success(QDMI_query_operation_property_double(
+            device, gate.c_str(), sites.data(), 1, QDMI_OPERATION_DURATION,
+            &duration)))
+            << "Failed to query duration for gate " << gate;
+        EXPECT_TRUE(QDMI_is_Success(QDMI_query_operation_property_double(
+            device, gate.c_str(), sites.data(), 1, QDMI_OPERATION_FIDELITY,
+            &fidelity)))
+            << "Failed to query fidelity for gate " << gate;
+      }
+    }
+    if (gate_num_qubits == 2) {
+      for (const auto &[control, target] : coupling_map) {
+        auto sites = std::array{control, target};
+        EXPECT_TRUE(QDMI_is_Success(QDMI_query_operation_property_double(
+            device, gate.c_str(), sites.data(), 2, QDMI_OPERATION_DURATION,
+            &duration)))
+            << "Failed to query duration for gate " << gate;
+        EXPECT_TRUE(QDMI_is_Success(QDMI_query_operation_property_double(
+            device, gate.c_str(), sites.data(), 2, QDMI_OPERATION_FIDELITY,
+            &fidelity)))
+            << "Failed to query fidelity for gate " << gate;
+      }
+    }
+  }
+}
+
+TEST_P(QDMIImplementationTest, ToolCompile) {
   Tool tool(device);
+  const auto num_qubits = Query_num_qubits(device);
+
   const std::string input = "OPENQASM 2.0;\n"
                             "include \"qelib1.inc\";\n"
                             "qreg q[2];\n"
@@ -172,7 +166,9 @@ TEST_F(QDMITest, ToolCompile) {
                             "cx q[0], q[1];\n";
   const std::string expected = "OPENQASM 2.0;\n"
                                "include \"qelib1.inc\";\n"
-                               "qreg q[5];\n"
+                               "qreg q[" +
+                               std::to_string(num_qubits) +
+                               "];\n"
                                "h q[0];\n"
                                "cx q[0], q[1];\n";
   const std::string actual = tool.compile(input);
