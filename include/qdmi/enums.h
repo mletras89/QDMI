a01ef1eafd450d528b59c93678003f998dc7fdef
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-10-21 10:45:12 +0200

    📝 Improve docs

--- include/qdmi/enums.h
+++ include/qdmi/enums.h
@@ -16,13 +16,12 @@ extern "C" {
 
 /// Enum of the device properties that can be queried.
 enum QDMI_DEVICE_PROPERTY_T {
-  QDMI_NAME = 0,           /**< `char*` (string) The name of the device. */
-  QDMI_DEVICE_VERSION = 1, /**< `char*` (string) The version of the device. */
-  QDMI_LIBRARY_VERSION =
-      2,               /**< `char*` (string) The implemented version of QDMI. */
-  QDMI_NUM_QUBITS = 3, /**< `int` The number of qubits in the device. */
-  QDMI_DEVICE_STATUS =
-      4, /**< `int` The @ref QDMI_Device_Status of the device. */
+  QDMI_NAME = 0,           ///< `char*` (string) The name of the device.
+  QDMI_DEVICE_VERSION = 1, ///< `char*` (string) The version of the device.
+  /// `char*` (string) The implemented version of QDMI.
+  QDMI_LIBRARY_VERSION = 2,
+  QDMI_NUM_QUBITS = 3,    ///< `int` The number of qubits in the device.
+  QDMI_DEVICE_STATUS = 4, ///< `int` The @ref QDMI_Device_Status of the device.
   /**
    * @brief `int*` (int list) The coupling map of the device.
    * @details The returned list contains pairs of qubits that are coupled. The
@@ -54,12 +53,12 @@ typedef enum QDMI_DEVICE_PROPERTY_T QDMI_Device_Property;
 
 /// Enum of the site properties that can be queried.
 enum QDMI_DEVICE_STATUS_T {
-  QDMI_DEVICE_OFFLINE = 0,     /**< The device is offline. */
-  QDMI_DEVICE_IDLE = 1,        /**< The device is idle. */
-  QDMI_DEVICE_BUSY = 2,        /**< The device is busy. */
-  QDMI_DEVICE_ERROR = 3,       /**< The device is in an error state. */
-  QDMI_DEVICE_MAINTENANCE = 4, /**< The device is in maintenance. */
-  QDMI_DEVICE_CALIBRATION = 5, /**< The device is in calibration. */
+  QDMI_DEVICE_OFFLINE = 0,     ///< The device is offline.
+  QDMI_DEVICE_IDLE = 1,        ///< The device is idle.
+  QDMI_DEVICE_BUSY = 2,        ///< The device is busy.
+  QDMI_DEVICE_ERROR = 3,       ///< The device is in an error state.
+  QDMI_DEVICE_MAINTENANCE = 4, ///< The device is in maintenance.
+  QDMI_DEVICE_CALIBRATION = 5, ///< The device is in calibration.
   /**
    * @brief The maximum value of the enum.
    * @details This value can be used for bounds checks by the backends.
@@ -74,8 +73,8 @@ typedef enum QDMI_DEVICE_STATUS_T QDMI_Device_Status;
 
 /// Enum of the site properties that can be queried.
 enum QDMI_SITE_PROPERTY_T {
-  QDMI_T1_TIME = 0, /**< `double` The T1 time of a site in µs. */
-  QDMI_T2_TIME = 1, /**< `double` The T2 time of a site in µs. */
+  QDMI_T1_TIME = 0, ///< `double` The T1 time of a site in µs.
+  QDMI_T2_TIME = 1, ///< `double` The T2 time of a site in µs.
   /**
    * @brief The maximum value of the enum.
    * @details This value can be used for bounds checks by the backends.
@@ -92,13 +91,9 @@ typedef enum QDMI_SITE_PROPERTY_T QDMI_Site_Property;
 enum QDMI_OPERATION_PROPERTY_T {
   /// `int` The number of qubits in the operation.
   QDMI_OPERATION_NUM_QUBITS = 0,
-  /**
-   * @brief `double` The duration of an operation in µs.
-   */
+  /// `double` The duration of an operation in µs.
   QDMI_OPERATION_DURATION = 1,
-  /**
-   * @brief `double` The fidelity of an operation.
-   */
+  /// `double` The fidelity of an operation.
   QDMI_OPERATION_FIDELITY = 2,
   /**
    * @brief The maximum value of the enum.
@@ -114,14 +109,14 @@ typedef enum QDMI_OPERATION_PROPERTY_T QDMI_Operation_Property;
 
 /// Enum of the device properties that can be queried.
 enum QDMI_JOB_STATUS_T {
-  QDMI_JOB_STATUS_SUBMITTED =
-      0, /**< The job was submitted and is waiting to be executed */
-  QDMI_JOB_STATUS_DONE =
-      1, /**< The job is done, and the result can be retrieved. */
-  QDMI_JOB_STATUS_RUNNING =
-      2, /**< The job is running, and the result is not yet available. */
-  QDMI_JOB_STATUS_CANCELLED =
-      3 /**< The job was cancelled and the result is not available. */
+  /// The job was submitted and is waiting to be executed
+  QDMI_JOB_STATUS_SUBMITTED = 0,
+  /// The job is done, and the result can be retrieved.
+  QDMI_JOB_STATUS_DONE = 1,
+  /// The job is running, and the result is not yet available.
+  QDMI_JOB_STATUS_RUNNING = 2,
+  /// The job was cancelled and the result is not available.
+  QDMI_JOB_STATUS_CANCELLED = 3
 };
 
 /// Type of the device status.
@@ -133,10 +128,8 @@ typedef enum QDMI_JOB_STATUS_T QDMI_Job_Status;
  * values must be powers of 2.
  */
 enum QDMI_DEVICE_MODE_T {
-  /// To open the device in read-only mode.
-  QDMI_DEVICE_MODE_READ_ONLY = 0,
-  /// To open the device in read-write mode.
-  QDMI_DEVICE_MODE_READ_WRITE = 1,
+  QDMI_DEVICE_MODE_READ_ONLY = 0,  ///< To open the device in read-only mode.
+  QDMI_DEVICE_MODE_READ_WRITE = 1, ///< To open the device in read-write mode.
 };
 
 /// Type of the device mode used in opening a device.
@@ -146,16 +139,16 @@ typedef enum QDMI_DEVICE_MODE_T QDMI_Device_Mode;
  * @brief Status codes returned by the API.
  */
 enum QDMI_STATUS {
-  QDMI_WARN_GENERAL = 1,             /**< A general warning. */
-  QDMI_SUCCESS = 0,                  /**< The operation was successful. */
-  QDMI_ERROR_FATAL = -1,             /**< A fatal error. */
-  QDMI_ERROR_OUT_OF_MEM = -2,        /**< Out of memory. */
-  QDMI_ERROR_NOT_IMPLEMENTED = -3,   /**< Not implemented. */
-  QDMI_ERROR_LIB_NOT_FOUND = -4,     /**< Library not found. */
-  QDMI_ERROR_NOT_FOUND = -5,         /**< Element not found. */
-  QDMI_ERROR_OUT_OF_RANGE = -6,      /**< Out of range. */
-  QDMI_ERROR_INVALID_ARGUMENT = -7,  /**< Invalid argument. */
-  QDMI_ERROR_PERMISSION_DENIED = -8, /**< Permission denied. */
+  QDMI_WARN_GENERAL = 1,             ///< A general warning.
+  QDMI_SUCCESS = 0,                  ///< The operation was successful.
+  QDMI_ERROR_FATAL = -1,             ///< A fatal error.
+  QDMI_ERROR_OUT_OF_MEM = -2,        ///< Out of memory.
+  QDMI_ERROR_NOT_IMPLEMENTED = -3,   ///< Not implemented.
+  QDMI_ERROR_LIB_NOT_FOUND = -4,     ///< Library not found.
+  QDMI_ERROR_NOT_FOUND = -5,         ///< Element not found.
+  QDMI_ERROR_OUT_OF_RANGE = -6,      ///< Out of range.
+  QDMI_ERROR_INVALID_ARGUMENT = -7,  ///< Invalid argument.
+  QDMI_ERROR_PERMISSION_DENIED = -8, ///< Permission denied.
 };
 
 #ifdef __cplusplus
