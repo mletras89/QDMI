839d3c4299ec879b2444026e3bc97e82ca9a75d5
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-10-24 22:32:40 +0200

    Revert "🔥 Remove templates"
    This reverts commit 3df8869c5c5d170c5ec77fa0e261c965103d2f67.

---
+++ templates/device/c/cmake/ExternalDependencies.cmake
@@ -0,0 +1,65 @@
+# ------------------------------------------------------------------------------
+# Part of the MQSS Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# ------------------------------------------------------------------------------
+
+include(FetchContent)
+include(CMakeDependentOption)
+set(FETCH_PACKAGES "")
+
+if(TARGET qdmi::qdmi)
+  message(STATUS "Device template: QDMI is already available.")
+else()
+  message(STATUS "Device template: QDMI will be included via FetchContent")
+  # cmake-format: off
+    set(QDMI_VERSION 0.2.0
+            CACHE STRING "QDMI version")
+    set(QDMI_REV "c4e06b974d9ddd35964f41d7e40a17dd0f4b3539"
+            CACHE STRING "QDMI identifier (tag, branch or commit hash)")
+    set(QDMI_REPO_OWNER "Munich-Quantum-Software-Stack"
+            CACHE STRING "QDMI repository owner (change when using a fork)")
+    # cmake-format: on
+
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
+    FetchContent_Declare(
+      qdmi
+      GIT_REPOSITORY https://github.com/${QDMI_REPO_OWNER}/qdmi.git
+      GIT_TAG ${QDMI_REV}
+      FIND_PACKAGE_ARGS ${QDMI_VERSION})
+    list(APPEND FETCH_PACKAGES qdmi)
+  else()
+    find_package(qdmi ${QDMI_VERSION} QUIET)
+    if(NOT qdmi_FOUND)
+      FetchContent_Declare(
+        qdmi
+        GIT_REPOSITORY https://github.com/${QDMI_REPO_OWNER}/qdmi.git
+        GIT_TAG ${QDMI_REV})
+      list(APPEND FETCH_PACKAGES qdmi)
+    endif()
+  endif()
+
+  set(gtest_force_shared_crt
+      ON
+      CACHE BOOL "" FORCE)
+  set(GTEST_VERSION
+      1.14.0
+      CACHE STRING "Google Test version")
+  set(GTEST_URL
+      https://github.com/google/googletest/archive/refs/tags/v${GTEST_VERSION}.tar.gz
+  )
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
+    FetchContent_Declare(googletest URL ${GTEST_URL} FIND_PACKAGE_ARGS
+                                        ${GTEST_VERSION} NAMES GTest)
+    list(APPEND FETCH_PACKAGES googletest)
+  else()
+    find_package(googletest ${GTEST_VERSION} QUIET NAMES GTest)
+    if(NOT googletest_FOUND)
+      FetchContent_Declare(googletest URL ${GTEST_URL})
+      list(APPEND FETCH_PACKAGES googletest)
+    endif()
+  endif()
+endif()
+
+# Make all declared dependencies available.
+FetchContent_MakeAvailable(${FETCH_PACKAGES})
---
+++ templates/device/cxx/src/my_device.cpp
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------------
+Part of the MQSS Project, under the Apache License v2.0 with LLVM Exceptions.
+See https://llvm.org/LICENSE.txt for license information.
+SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+------------------------------------------------------------------------------*/
+
+/** @file
+ * @brief A template of a device implementation in C.
+ * @details In the end, all QDMI_ERROR_NOT_IMPLEMENTED return codes should be
+ * replaced by QDMI_ERROR_NOT_IMPLEMENTED return codes.
+ * For the documentation of the functions, see the official documentation.
+ */
+
+#include "qdmi/device.h"
+
+/**
+ * @brief Implementation of the QDMI_Job structure.
+ * @details This structure can, e.g., be used to store the job id.
+ */
+typedef struct QDMI_Job_impl_d {
+} QDMI_Job_impl_t;
+
+int QDMI_query_get_sites_dev(int num_entries, QDMI_Site *sites,
+                             int *num_sites) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_query_get_operations_dev(int num_entries, QDMI_Operation *operations,
+                                  int *num_operations) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_query_device_property_dev(QDMI_Device_Property prop, int size,
+                                   void *value, int *size_ret) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_query_site_property_dev(int site, QDMI_Site_Property prop, int size,
+                                 void *value, int *size_ret) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_query_operation_property_dev(const char *operation, int num_sites,
+                                      const int *sites,
+                                      QDMI_Operation_Property prop, int size,
+                                      void *value, int *size_ret) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_control_submit_qasm_dev(const char *qasm_string, int num_shots,
+                                 QDMI_Job *job) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_control_submit_qir_string_dev(const char *qir_string, int num_shots,
+                                       QDMI_Job *job) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_control_submit_qir_module_dev(const void *qir_string, int num_shots,
+                                       QDMI_Job *job) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_control_cancel_dev(QDMI_Job job) { return QDMI_ERROR_NOTIMPLEMENTED; }
+
+int QDMI_control_check_dev(QDMI_Job job, QDMI_Job_Status *status) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_control_wait_dev(QDMI_Job job) { return QDMI_ERROR_NOTIMPLEMENTED; }
+
+int QDMI_control_get_hist_dev(QDMI_Job job, char ***data, int **counts,
+                              int *size) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int QDMI_control_get_raw_dev(QDMI_Job job, char ***data, int *size) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+void QDMI_control_free_job_dev(QDMI_Job job) { delete job; }
+
+int QDMI_control_initialize_dev(void) { return QDMI_ERROR_NOTIMPLEMENTED; }
+
+int QDMI_control_finalize_dev(void) { return QDMI_ERROR_NOTIMPLEMENTED; }
