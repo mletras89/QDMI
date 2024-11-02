6383f775f63d951ac2415b49d60e50fe270a89e6
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-11-02 11:07:18 +0100

    ♻️ Update template tests, prefix test_defs, link qdmi in all devices

--- templates/device/src/my_device.cpp
+++ templates/device/src/my_device.cpp
@@ -11,7 +11,7 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  * For the documentation of the functions, see the official documentation.
  */
 
-#include "qdmi/device.h"
+#include "my_device.h"
 
 // The following line ignores the unused parameters in the functions.
 // Please remove the following code block after populating the functions.
@@ -24,82 +24,88 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #endif
 /**
- * @brief Implementation of the QDMI_Job structure.
+ * @brief Implementation of the MY_QDMI_Job structure.
  * @details This structure can, e.g., be used to store the job id.
  */
-struct QDMI_Job_impl_d {};
+struct MY_QDMI_Job_impl_d {};
 
 /**
- * @brief Implementation of the QDMI_Site structure.
+ * @brief Implementation of the MY_QDMI_Site structure.
  * @details This structure can, e.g., be used to store the site id.
  */
-struct QDMI_Site_impl_d {};
+struct MY_QDMI_Site_impl_d {};
 
 /**
- * @brief Implementation of the QDMI_Operation structure.
+ * @brief Implementation of the MY_QDMI_Operation structure.
  * @details This structure can, e.g., be used to store the operation id.
  */
-struct QDMI_Operation_impl_d {};
+struct MY_QDMI_Operation_impl_d {};
 
-int QDMI_query_get_sites_dev(int num_entries, QDMI_Site *sites,
-                             int *num_sites) {
+int MY_QDMI_query_get_sites_dev(int num_entries, MY_QDMI_Site *sites,
+                                int *num_sites) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-int QDMI_query_get_operations_dev(int num_entries, QDMI_Operation *operations,
-                                  int *num_operations) {
+int MY_QDMI_query_get_operations_dev(int num_entries,
+                                     MY_QDMI_Operation *operations,
+                                     int *num_operations) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-int QDMI_query_device_property_dev(QDMI_Device_Property prop, int size,
-                                   void *value, int *size_ret) {
-  return QDMI_ERROR_NOTIMPLEMENTED;
-}
-
-int QDMI_query_site_property_dev(QDMI_Site site, QDMI_Site_Property prop,
-                                 int size, void *value, int *size_ret) {
-  return QDMI_ERROR_NOTIMPLEMENTED;
-}
-
-int QDMI_query_operation_property_dev(QDMI_Operation operation, int num_sites,
-                                      const QDMI_Site *sites,
-                                      QDMI_Operation_Property prop, int size,
+int MY_QDMI_query_device_property_dev(QDMI_Device_Property prop, int size,
                                       void *value, int *size_ret) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-int QDMI_control_create_job_dev(QDMI_Program_Format format, int size,
-                                const void *prog, QDMI_Job *job) {
+int MY_QDMI_query_site_property_dev(MY_QDMI_Site site, QDMI_Site_Property prop,
+                                    int size, void *value, int *size_ret) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-int QDMI_control_set_parameter_dev(QDMI_Job job, QDMI_Job_Parameter param,
-                                   int size, const void *value) {
+int MY_QDMI_query_operation_property_dev(MY_QDMI_Operation operation,
+                                         int num_sites,
+                                         const MY_QDMI_Site *sites,
+                                         QDMI_Operation_Property prop, int size,
+                                         void *value, int *size_ret) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-int QDMI_control_submit_job_dev(QDMI_Job job) {
+int MY_QDMI_control_create_job_dev(QDMI_Program_Format format, int size,
+                                   const void *prog, MY_QDMI_Job *job) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-int QDMI_control_cancel_dev(QDMI_Job job) { return QDMI_ERROR_NOTIMPLEMENTED; }
-
-int QDMI_control_check_dev(QDMI_Job job, QDMI_Job_Status *status) {
+int MY_QDMI_control_set_parameter_dev(MY_QDMI_Job job, QDMI_Job_Parameter param,
+                                      int size, const void *value) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-int QDMI_control_wait_dev(QDMI_Job job) { return QDMI_ERROR_NOTIMPLEMENTED; }
-
-int QDMI_control_get_data_dev(QDMI_Job job, QDMI_Job_Result result, int size,
-                              void *data, int *size_ret) {
+int MY_QDMI_control_submit_job_dev(MY_QDMI_Job job) {
   return QDMI_ERROR_NOTIMPLEMENTED;
 }
 
-void QDMI_control_free_job_dev(QDMI_Job job) {}
+int MY_QDMI_control_cancel_dev(MY_QDMI_Job job) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
 
-int QDMI_control_initialize_dev() { return QDMI_ERROR_NOTIMPLEMENTED; }
+int MY_QDMI_control_check_dev(MY_QDMI_Job job, QDMI_Job_Status *status) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
 
-int QDMI_control_finalize_dev() { return QDMI_ERROR_NOTIMPLEMENTED; }
+int MY_QDMI_control_wait_dev(MY_QDMI_Job job) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+int MY_QDMI_control_get_data_dev(MY_QDMI_Job job, QDMI_Job_Result result,
+                                 int size, void *data, int *size_ret) {
+  return QDMI_ERROR_NOTIMPLEMENTED;
+}
+
+void MY_QDMI_control_free_job_dev(MY_QDMI_Job job) {}
+
+int MY_QDMI_control_initialize_dev() { return QDMI_ERROR_NOTIMPLEMENTED; }
+
+int MY_QDMI_control_finalize_dev() { return QDMI_ERROR_NOTIMPLEMENTED; }
 
 // The following line ignores the unused parameters in the functions.
 // Please remove the following code block after populating the functions.
