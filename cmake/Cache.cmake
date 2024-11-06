0627be2f2895a5a9c6d0f53412e359e5e2629890
burgholzer <burgholzer@me.com>
2024-11-06 16:27:28 +0100

    ♻️ streamline CMake structure and reduce code duplication
    Signed-off-by: burgholzer <burgholzer@me.com>

--- cmake/Cache.cmake
+++ cmake/Cache.cmake
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------------
+# Part of the MQSS Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# ------------------------------------------------------------------------------
+
 option(ENABLE_CACHE "Enable compiler cache if available" ON)
 if(NOT ENABLE_CACHE)
   return()
