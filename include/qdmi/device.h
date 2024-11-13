f5dcdb1c80328609454aa9480c3529817619ddb3
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-11-13 09:02:07 +0100

    📄 Fix reference to license in file header

--- include/qdmi/device.h
+++ include/qdmi/device.h
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
 Part of the MQSS Project, under the Apache License v2.0 with LLVM Exceptions.
-See https://llvm.org/LICENSE.txt for license information.
-SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+See https://github.com/Munich-Quantum-Software-Stack/QDMI/blob/develop/LICENSE
+for license information. SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 ------------------------------------------------------------------------------*/
 
 /** @file
